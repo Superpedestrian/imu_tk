504443e0e3b3f83861660fcc700893e32a22f37d
Ben Segal <ben@navmatic.io>
2021-11-18 11:34:06 -0800

    Add a mac build script.

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!bash
+
+brew install cmake ceres-solver eigen boost cartr/qt4/pyqt@4
+brew unlink qt
+brew link --overwrite qt@4
+
+sudo ln -s /usr/local/include/eigen3/Eigen /usr/local/include/Eigen
+
+cd imu_tk
+mkdir build
+cd build
+cmake  ..
+make
